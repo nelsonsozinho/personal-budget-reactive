4ce2bd08f301839b70bc4f67ee74d0df1a66f608
Nelson Sozinho <nelsonsozinho@gmail.com>
2023-01-17 10:53:18 -0400

    account to bill

--- src/main/resources/db/migration/V11__rename_account_to_bill.sql
+++ src/main/resources/db/migration/V11__rename_account_to_bill.sql
@@ -1,1 +1,6 @@
-alter table public.account rename to bill;
+alter table if exists public.account rename to bill;
+alter table if exists public.bill rename column account_id to bill_id;
+alter table if exists public.payment rename column account_id to bill_id;
+
+alter table if exists public.payment drop constraint account_account_id_fk;
+alter table if exists public.payment add constraint bill_bill_id_fk foreign key (bill_id) references public.bill (bill_id);

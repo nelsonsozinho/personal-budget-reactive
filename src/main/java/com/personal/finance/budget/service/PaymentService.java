06150c5ae772db2f87d94af805ca2dbe5828b614
Nelson Sozinho <nelsonsozinho@gmail.com>
2023-01-17 18:53:37 -0400

    fix the relational data after rename

--- src/main/java/com/personal/finance/budget/service/PaymentService.java
+++ src/main/java/com/personal/finance/budget/service/PaymentService.java
@@ -3,15 +3,12 @@ package com.personal.finance.budget.service;
 import com.personal.finance.budget.controller.request.PaymentRequest;
 import com.personal.finance.budget.controller.response.PaymentResponse;
 import com.personal.finance.budget.mapper.PaymentMapper;
-import com.personal.finance.budget.model.Bill;
 import com.personal.finance.budget.model.Payment;
-import com.personal.finance.budget.repository.BillRepository;
 import com.personal.finance.budget.repository.PaymentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -21,8 +18,6 @@ import java.util.UUID;
 public class PaymentService {
 
     private final PaymentRepository paymentRepository;
-
-    private final BillRepository billRepository;
 
     private final PaymentMapper paymentMapper;
 
--- src/main/resources/db/migration/V11__rename_account_to_bill.sql
+++ src/main/resources/db/migration/V11__rename_account_to_bill.sql
@@ -4,3 +4,9 @@ alter table if exists public.payment rename column account_id to bill_id;
 
 alter table if exists public.payment drop constraint account_account_id_fk;
 alter table if exists public.payment add constraint bill_bill_id_fk foreign key (bill_id) references public.bill (bill_id);
+
+alter table public.account_cost_center rename column account_id to bill_id;
+alter table public.account_cost_center drop constraint account_cost_center_account_fk;
+alter table public.account_cost_center rename constraint account_cost_center_cost_center_fk to bill_cost_center_cost_center_fk;
+alter table public.account_cost_center rename to bill_cost_center;
+alter table public.bill_cost_center add constraint bill_cost_center_account_fk foreign key (bill_id) references public.bill;

def284e6133cca8501f5fe73e721d7baa97ff27a
nelsonsozinho <nelsonsozinho@gmail.com>
2023-01-24 09:28:51 -0400

    improovind the response object from Health endpoint

--- src/main/java/com/personal/finance/budget/controller/HealthController.java
+++ src/main/java/com/personal/finance/budget/controller/HealthController.java
@@ -1,14 +1,17 @@
 package com.personal.finance.budget.controller;
 
+import com.personal.finance.budget.controller.response.HealthResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class HealthController {
 
     @GetMapping("/health")
-    public String health() {
-        return "ok";
+    public Mono<HealthResponse> health() {
+        return Mono.just(HealthResponse.builder())
+                .map(builder -> builder.status("Ok").build());
     }
 
 }
---
+++ src/main/java/com/personal/finance/budget/controller/response/HealthResponse.java
@@ -0,0 +1,13 @@
+package com.personal.finance.budget.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class HealthResponse {
+    private String status;
+
+}
